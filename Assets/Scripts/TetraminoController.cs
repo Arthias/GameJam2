fc8593ea7d54d33a64041ee1edb79e7367c90d28
Bruno Manfredi <bruno.manfredi@bbva.com>
2021-12-27 00:23:59 -0300

    Gamejam proj upload

---
+++ Assets/Scripts/TetraminoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetraminoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movemet();
+
+
+    }
+
+    void Movemet()
+    {
+        this.transform.Translate(0, -1, 0);
+        
+    }
+}
